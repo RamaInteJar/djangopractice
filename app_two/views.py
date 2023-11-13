6a8899242d73eccdf72a976676d1719dae832270
pentiumKing <ramahawai87@gmail.com>
2023-11-13 17:45:24 -0500

    initial commit

---
+++ app_two/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<em>My Second App</em>")
+
+def help(request):
+    helpdict = {'help_insert': "HELP PAGE"}    
+    return render(request, 'app_two/help.html', context=helpdict)
+
+# Create your views here.
